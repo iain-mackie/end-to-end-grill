593388a17f960d00c9914526fb08edadeabb5a92
Iain Mackie <iain@Iains-MacBook-Pro.local>
2021-02-01 17:46:31 +0000

    addding execption print and docker

--- docker/build_gpu_job.sh
+++ docker/build_gpu_job.sh
@@ -1,11 +1,11 @@
 # build docker
-DOCKER_PATH="./Dockerfile_gpu"
+DOCKER_PATH="./Dockerfile_gpu_job"
 
 echo "Trying to build docker image from:" $DOCKER_PATH;
 
 if [ -f "$DOCKER_PATH" ]; then
   echo "file exits"
-  sudo docker build -t iainmackie/end-to-end-grill-gpu:v4 -f $DOCKER_PATH .
+  sudo docker build -t iainmackie/end-to-end-grill-gpu-job:v4 -f $DOCKER_PATH .
 
 else
   echo "Error - path to file not found:" $DOCKER_PATH;
